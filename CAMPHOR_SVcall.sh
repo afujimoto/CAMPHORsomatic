f7d42b844fe5a7110b2506ba4695d562b13d1248
Akihiro Fujimoto <afujimoto@m.u-tokyo.ac.jp>
2021-03-03 14:06:32 +0900

    Update CAMPHOR_SVcall.sh

--- CAMPHOR_SVcall.sh
+++ CAMPHOR_SVcall.sh
@@ -12,25 +12,25 @@ SRC=./src
 DATA=./data
 
 if [ ! -d $OUTPUT ]; then mkdir $OUTPUT; fi
-echo "$SAMTOOLS view $BAM|python $SRC/read_format.py /dev/stdin $MIN_INDEL_LENGTH 0.3|python $SRC/SV_format.py /dev/stdin 0.05 $MIN_INDEL_LENGTH 0 10 0.3 1000 20 > $OUTPUT/SV_read.txt"
+#echo "$SAMTOOLS view $BAM|python $SRC/read_format.py /dev/stdin $MIN_INDEL_LENGTH 0.3|python $SRC/SV_format.py /dev/stdin 0.05 $MIN_INDEL_LENGTH 0 10 0.3 1000 20 > $OUTPUT/SV_read.txt"
 $SAMTOOLS view $BAM|python3 $SRC/read_format.py /dev/stdin $MIN_INDEL_LENGTH 0.3|python3 $SRC/SV_format.py /dev/stdin 0.05 $MIN_INDEL_LENGTH 0 10 0.3 1000 20 > $OUTPUT/SV_read.txt
 
-echo "python $SRC/SV_candidate.py $OUTPUT/SV_read.txt|sort -k2,2g -k3,3 -k4,4g|perl $SRC/SV_format_change.5.pl /dev/stdin > $OUTPUT/SV_read.txt.2"
+#echo "python $SRC/SV_candidate.py $OUTPUT/SV_read.txt|sort -k2,2g -k3,3 -k4,4g|perl $SRC/SV_format_change.5.pl /dev/stdin > $OUTPUT/SV_read.txt.2"
 python3 $SRC/SV_candidate.py $OUTPUT/SV_read.txt|sort -k2,2g -k3,3 -k4,4g|perl $SRC/SV_format_change.5.pl /dev/stdin > $OUTPUT/SV_read.txt.2
 
-echo "python $SRC/SV_candidate_INS.py $OUTPUT/SV_read.txt 100000|sort -k2,2g -k3,3 -k4,4g|perl $SRC/SV_format_change.5.pl /dev/stdin|python $SRC/SV_support_read.py /dev/stdin 50 0.2 20 50 1 30 0.3|sort -k1,1 -k2,2g|python $SRC/merge_BP_INS.py /dev/stdin 300 0.8|sort -k1,1 -k2,2g > $OUTPUT/INS_candidate.txt"
+#echo "python $SRC/SV_candidate_INS.py $OUTPUT/SV_read.txt 100000|sort -k2,2g -k3,3 -k4,4g|perl $SRC/SV_format_change.5.pl /dev/stdin|python $SRC/SV_support_read.py /dev/stdin 50 0.2 20 50 1 30 0.3|sort -k1,1 -k2,2g|python $SRC/merge_BP_INS.py /dev/stdin 300 0.8|sort -k1,1 -k2,2g > $OUTPUT/INS_candidate.txt"
 python3 $SRC/SV_candidate_INS.py $OUTPUT/SV_read.txt 100000|sort -k2,2g -k3,3 -k4,4g|perl $SRC/SV_format_change.5.pl /dev/stdin|python3 $SRC/SV_support_read.py /dev/stdin 50 0.2 20 50 1 30 0.3|sort -k1,1 -k2,2g|python3 $SRC/merge_BP_INS.py /dev/stdin 300 0.8|sort -k1,1 -k2,2g > $OUTPUT/INS_candidate.txt
 
-echo "python $SRC/SV_support_read.2.py $OUTPUT/SV_read.txt.2 50 DEL|sort -k1,1 -k2,2g -k3,3 -k4,4g|python $SRC/merge_BP.py /dev/stdin 1000 0.8|sort -k1,1 -k2,2g -k3,3 -k4,4g > $OUTPUT/DEL_candidate.txt"
+#echo "python $SRC/SV_support_read.2.py $OUTPUT/SV_read.txt.2 50 DEL|sort -k1,1 -k2,2g -k3,3 -k4,4g|python $SRC/merge_BP.py /dev/stdin 1000 0.8|sort -k1,1 -k2,2g -k3,3 -k4,4g > $OUTPUT/DEL_candidate.txt"
 python3 $SRC/SV_support_read.2.py $OUTPUT/SV_read.txt.2 50 DEL|sort -k1,1 -k2,2g -k3,3 -k4,4g|python3 $SRC/merge_BP.py /dev/stdin 1000 0.8|sort -k1,1 -k2,2g -k3,3 -k4,4g > $OUTPUT/DEL_candidate.txt
 
-echo "python $SRC/SV_support_read.2.py $OUTPUT/SV_read.txt.2 1500 CHR > $OUTPUT/CHR_candidate.txt"
+#echo "python $SRC/SV_support_read.2.py $OUTPUT/SV_read.txt.2 1500 CHR > $OUTPUT/CHR_candidate.txt"
 python3 $SRC/SV_support_read.2.py $OUTPUT/SV_read.txt.2 1500 CHR > $OUTPUT/CHR_candidate.txt
 
-echo "python $SRC/SV_support_read.2.py $OUTPUT/SV_read.txt.2 100 DIF|sort -k1,1 -k2,2g -k3,3 -k4,4g|python $SRC/merge_BP.py /dev/stdin 1000 0.8|sort -k1,1 -k2,2g -k3,3 -k4,4g > $OUTPUT/INV_candidate.txt"
+#echo "python $SRC/SV_support_read.2.py $OUTPUT/SV_read.txt.2 100 DIF|sort -k1,1 -k2,2g -k3,3 -k4,4g|python $SRC/merge_BP.py /dev/stdin 1000 0.8|sort -k1,1 -k2,2g -k3,3 -k4,4g > $OUTPUT/INV_candidate.txt"
 python3 $SRC/SV_support_read.2.py $OUTPUT/SV_read.txt.2 100 DIF|sort -k1,1 -k2,2g -k3,3 -k4,4g|python3 $SRC/merge_BP.py /dev/stdin 1000 0.8|sort -k1,1 -k2,2g -k3,3 -k4,4g > $OUTPUT/INV_candidate.txt
 
-echo "python $SRC/SV_support_read.2.py $OUTPUT/SV_read.txt.2 100 TRS|sort -k1,1 -k2,2g -k3,3 -k4,4g|python $SRC/merge_BP.py /dev/stdin 1000 0.8|sort -k1,1 -k2,2g -k3,3 -k4,4g > $OUTPUT/TRS_candidate.txt"
+#echo "python $SRC/SV_support_read.2.py $OUTPUT/SV_read.txt.2 100 TRS|sort -k1,1 -k2,2g -k3,3 -k4,4g|python $SRC/merge_BP.py /dev/stdin 1000 0.8|sort -k1,1 -k2,2g -k3,3 -k4,4g > $OUTPUT/TRS_candidate.txt"
 python3 $SRC/SV_support_read.2.py $OUTPUT/SV_read.txt.2 100 TRS|sort -k1,1 -k2,2g -k3,3 -k4,4g|python3 $SRC/merge_BP.py /dev/stdin 1000 0.8|sort -k1,1 -k2,2g -k3,3 -k4,4g > $OUTPUT/TRS_candidate.txt
 
 rm $OUTPUT/SV_read.txt $OUTPUT/SV_read.txt.2
